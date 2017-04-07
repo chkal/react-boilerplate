0c699d4dd3ab796fe4cc1bd636e248d608103c90
Christian Kaltepoth <christian@kaltepoth.de>
2017-04-07 07:38:54 +0200

    Removed empty constructor

--- src/main/javascript/hello/hello.tsx
+++ src/main/javascript/hello/hello.tsx
@@ -3,10 +3,6 @@ import ReactDOM = require("react-dom");
 
 export class Hello extends React.Component<Props,void> {
 
-  constructor( props: Props, context: any ) {
-    super( props, context );
-  }
-
   render(): JSX.Element|any {
     return (
       <h1>Hello, {this.props.name}!</h1>
